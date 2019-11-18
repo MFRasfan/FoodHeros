c81e7f5d0a4186c597238f0d5b4d8eb952765aa3
Rasfan M Faiz <rasfanfaiz@gmail.com>
2019-11-17 20:57:05 -0500

    chanegs

--- server.js
+++ server.js
@@ -24,35 +24,35 @@ app.use(pino);
 
 //twillio
 
-require('dotenv').config();
-const client = require('twilio')(
-    process.env.TWILIO_ACCOUT_SID,
-    process.env.TWILIO_AUTH_TOKEN)
-    //twillio
+// require('dotenv').config();
+// const client = require('twilio')(
+//     process.env.TWILIO_ACCOUT_SID,
+//     process.env.TWILIO_AUTH_TOKEN)
+//     //twillio
   
 
-app.get('/api/greeting', (req, res) => {
-  const name = req.query.name || 'World';
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
-});
+// app.get('/api/greeting', (req, res) => {
+//   const name = req.query.name || 'World';
+//   res.setHeader('Content-Type', 'application/json');
+//   res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+// });
 
-app.post('/api/messages', (req, res) => {
-  res.header('Content-Type', 'application/json');
-  client.messages
-    .create({
-      from: process.env.TWILIO_PHONE_NUMBER,
-      to: req.body.to,
-      body: req.body.body
-    })
-    .then(() => {
-      res.send(JSON.stringify({ success: true }));
-    })
-    .catch(err => {
-      console.log(err);
-      res.send(JSON.stringify({ success: false }));
-    });
-});
+// app.post('/api/messages', (req, res) => {
+//   res.header('Content-Type', 'application/json');
+//   client.messages
+//     .create({
+//       from: process.env.TWILIO_PHONE_NUMBER,
+//       to: req.body.to,
+//       body: req.body.body
+//     })
+//     .then(() => {
+//       res.send(JSON.stringify({ success: true }));
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       res.send(JSON.stringify({ success: false }));
+//     });
+// });
 
 // DB Config
 const db = require("./config/keys").mongoURI;
